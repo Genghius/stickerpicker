662367afb530f4af150eeaa1fdf97acca59b3efb
Genghius <genghius.khanate03@gmail.com>
2021-04-22 15:21:22 +0100

    OwOUwU

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = 'ba009627'
+version = '0.1.0+dev.ba009627'
+linkified_version = '0.1.0+dev.[ba009627](https://github.com/maunium/stickerpicker/commit/ba0096275ce966107b9a00f42e9894af74cebb4c)'
